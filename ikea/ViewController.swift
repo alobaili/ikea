4ccda39e6aceffcdf356c6508e11554674c7fbb2
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-07-02 11:04:02 +0300

    Initial Commit

---
+++ ikea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ikea
+//
+//  Created by Abdulaziz AlObaili on 02/07/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
